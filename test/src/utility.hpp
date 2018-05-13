9e1b9016469e7a32610f016d78d6e18d471fdc63
John Boyer <john.boyer@tutanota.com>
2018-05-13 13:02:34 -0400

    do while for macro

--- test/src/utility.hpp
+++ test/src/utility.hpp
@@ -4,13 +4,14 @@
 #include "rapid_string.h"
 #include <catch.hpp>
 
-#define CMP_STR(s, cmp)					   \
-	REQUIRE(rs_size(s) == cmp.length());		   \
-	REQUIRE(rs_data(s) == cmp);			   \
-							   \
-	if (rs_is_heap(s))				   \
-		REQUIRE(rs_capacity(s) >= cmp.length());	   \
-	else						   \
-		REQUIRE(rs_capacity(s) == RS_STACK_CAPACITY)
+#define CMP_STR(s, cmp) do {					\
+	REQUIRE(rs_size(s) == cmp.length());			\
+	REQUIRE(rs_data(s) == cmp);				\
+								\
+	if (rs_is_heap(s))					\
+		REQUIRE(rs_capacity(s) >= cmp.length());	\
+	else							\
+		REQUIRE(rs_capacity(s) == RS_STACK_CAPACITY);	\
+while (0)
 
 #endif // !UTILITY_HPP_ECB97D42D011D625
