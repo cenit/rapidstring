73307324810dfa57639222201d4b3ef7230a2c7f
John Boyer <john.boyer@tutanota.com>
2018-05-15 19:37:53 -0400

    avoid signed/unsigned mismatch

--- test/src/iterator.cpp
+++ test/src/iterator.cpp
@@ -12,8 +12,9 @@ TEST_CASE("Iterators")
 	rs_init_w(&s1, first.data());
 	auto it1 = rs_begin(&s1);
 	auto end1 = rs_end(&s1);
+	auto dist1 = std::distance(first.begin(), first.end());
 
-	REQUIRE(std::distance(it1, end1) == rs_size(&s1));
+	REQUIRE(std::distance(it1, end1) == dist1);
 
 	for (std::size_t i = 0; it1 != end1; it1++, i++)
 		REQUIRE(*it1 == first.at(i));
@@ -22,8 +23,9 @@ TEST_CASE("Iterators")
 	rs_init_w(&s2, second.data());
 	auto it2 = rs_begin(&s2);
 	auto end2 = rs_end(&s2);
+	auto dist2 = std::distance(second.begin(), second.end());
 
-	REQUIRE(std::distance(it2, end2) == rs_size(&s2));
+	REQUIRE(std::distance(it2, end2) == dist2);
 
 	for (std::size_t i = 0; it2 != end2; it2++, i++)
 		REQUIRE(*it2 == second.at(i));
