99f69d13b7675a686bf9be6fbf2e5f74edda6360
John Boyer <john.boyer@tutanota.com>
2018-05-15 09:59:42 -0400

    no newlines in macros

--- test/src/iterator.cpp
+++ test/src/iterator.cpp
@@ -13,8 +13,7 @@ TEST_CASE("Iterators")
 	auto it1 = rs_begin(&s1);
 	auto end1 = rs_end(&s1);
 
-	REQUIRE(std::distance(it1, end1) ==
-		std::distance(first.begin(), first.end());
+	REQUIRE(std::distance(it1, end1) == rs_size(&s1));
 
 	for (std::size_t i = 0; it1 != end1; it1++, i++)
 		REQUIRE(*it1 == first.at(i));
@@ -24,8 +23,7 @@ TEST_CASE("Iterators")
 	auto it2 = rs_begin(&s2);
 	auto end2 = rs_end(&s2);
 
-	REQUIRE(std::distance(it2, end2) ==
-		std::distance(second.begin(), second.end());
+	REQUIRE(std::distance(it2, end2) == rs_size(&s2));
 
 	for (std::size_t i = 0; it2 != end2; it2++, i++)
 		REQUIRE(*it2 == second.at(i));
