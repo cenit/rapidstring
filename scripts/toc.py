294703e658c752cbeafdce30f70f0153690b59a6
John <john.boyer@tutanota.com>
2018-07-22 19:42:04 -0500

    rewording on malloc failures

--- scripts/toc.py
+++ scripts/toc.py
@@ -2,6 +2,7 @@ from collections import OrderedDict
 
 # TODO: Allow this method to be run from any working directory.
 # TODO: Don't require this to be ran twice if a section is removed.
+# TODO: Move the data out of this file.
 
 header = '''/*
  * rapidstring - Maybe the fastest string library ever.
@@ -24,7 +25,7 @@ docs = '''/**
  *
  * All functions are named based off what state the string must be. For example, `rs_heap_cat_n` may only be used to concatenate strings that are on the heap. This method is intended for internal use. However, if you know for a fact your string is larger than #RS_STACK_CAPACITY, you may use the heap methods directly to avoid a flag check and obtain a minor speedup. The same is true for `rs_stack_cat_n`, and all other stack or heap specific functions. Any functions without either infix simply means it can handle the string in any state. These are the functions intended for the average user. Furthermore, the latter functions will grow the size of the string when required, while the former functions require this to be done manually.
  *
- * There are no error codes returned from this library, as nothing ever fails. The only possible sources of failure are invalid arguments or a failed allocation. The former is undefined behavior (or assert failures in when `NDEBUG` is not defined) and the latter may be verified with `errno`. The simple truth is that nearly all applications brutally fail either way when memory runs out, and `rapidstring` takes advantage of this by never checking whether allocations succeed. If your application must handle allocation failures, you may set `errno` to `0` before calling a function that either intializes or grows the heap buffer, and then check `errno` after this call. Nearly all modern compilers will set `errno` inside `malloc()` if it fails.
+ * There are no error codes returned from this library, as nothing ever fails. The only possible sources of failure are invalid arguments or a failed allocation. The former is undefined behavior (or assert failures in when `NDEBUG` is not defined) and the latter may be verified with `errno`. The simple truth is that nearly all applications brutally fail either way when memory runs out, and `rapidstring` takes advantage of this by never checking whether allocations succeed. If your application must handle allocation failures, you may set `errno` to `0` before calling a function that either intializes or grows the heap buffer, and then check `errno` after this call. All modern compilers will set `errno` if `malloc()` fails.
  *
  * @copyright Copyright © 2018 John Boyer.
  * @author <a href="https://github.com/boyerjohn">John Boyer</a>
